e01a3edee73138a5f8221d13a6fbd2b2ed025d06
sawa <sw41dc@gmail.com>
2020-05-24 17:35:10 +0900

    Add UsersController

--- cmd/web_dev_portal/main.go
+++ cmd/web_dev_portal/main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"text/template"
+
+	"github.com/daichi5/web_dev_portal/internal/controllers"
 )
 
 type PageData struct {
@@ -12,13 +14,14 @@ type PageData struct {
 var templates = template.Must(template.ParseFiles("internal/views/home.html"))
 
 func viewHomeHandler(w http.ResponseWriter, r *http.Request) {
-	err := templates.ExecuteTemplate(w, "home.html", PageData{Message: "this is message"})
+	err := templates.ExecuteTemplate(w, "home.html", PageData{Message: "this is a message"})
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
 
 func main() {
+	http.HandleFunc("/users/", controllers.UsersHandler)
 	http.HandleFunc("/", viewHomeHandler)
 	http.ListenAndServe(":8080", nil)
 }
---
+++ internal/controllers/users.go
@@ -0,0 +1,45 @@
+package controllers
+
+import (
+	"encoding/json"
+	"net/http"
+	"regexp"
+)
+
+type User struct {
+	ID   int    `json:"id"`
+	Name string `json:"name"`
+}
+
+var validPath = regexp.MustCompile("^/users/([0-9]+)$")
+
+func UsersHandler(w http.ResponseWriter, r *http.Request) {
+	m := validPath.FindStringSubmatch(r.URL.Path)
+	if len(m) > 0 {
+		show(w, r)
+	} else {
+		index(w, r)
+	}
+
+}
+
+func show(w http.ResponseWriter, r *http.Request) {
+	json, err := json.Marshal(User{ID: 1, Name: r.URL.Path})
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	users := []User{}
+	users = append(users, User{ID: 1, Name: "tom"})
+	users = append(users, User{ID: 2, Name: "alice"})
+	json, err := json.Marshal(users)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(json)
+}
